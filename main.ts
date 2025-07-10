3af851a4ed761faf36d8a4f5ccefe4af1fa3e313
rhotep <andreas.pittrich@posteo.de>
2025-07-10 20:01:29 +0200

    added pup and separate script for multiple instances

--- main.ts
+++ main.ts
@@ -1,69 +1,55 @@
-import { MappoBackend, type Adapter, AdapterMeta, scheduleFromCron, DenoKvStorage, NaiveMappoDiffer, getOakRouter, Item, LogSubject  } from "@mappo-aggregato/mappo-aggregato"
-import { Observable } from "rxjs"
-import { Application, Context, Next } from "@oak/oak"
+import	{ 
+			type Adapter, 
+			MappoBackend, 
+			DenoKvStorage, 
+			NaiveMappoDiffer, 
+			getOakRouter, 
+			LogSubject  
+		} 								from "@mappo-aggregato/mappo-aggregato/backend"
+import	{ 
+			Application, 
+			Context, 
+			Next 
+		} 								from "@oak/oak"
 
-class InfoCompassAdapter implements Adapter {
 
-	protected url	:	string
+export async function icRunMappo(
+	instanceName	: string,
+	storageName		: string,
+	adapters		: Adapter[],
+	port			: number,
+){
 
-	public meta 	:	AdapterMeta	
-	public schedule	:	Observable<void>
 
-	constructor(config : { meta: AdapterMeta, url: string, schedule: Observable<void>} ){
-		this.meta 		= config.meta
-		this.url 		= config.url
-		this.schedule 	= config.schedule
-	}	
+	const mappo 	= 	new MappoBackend({
+							storage			: new DenoKvStorage(storageName),
+							differ			: new NaiveMappoDiffer(),
+							adapters		: adapters,
+						})
 
-	async collectAndAdapt() : Promise<Item[]> {
-		const result 	= await fetch(this.url)
-		const data		= await result.json()
+	const log		=	new LogSubject()
 
-		return data.items	 
-	}			
+	log.importLogsFrom(mappo.log$, instanceName)
+	log.importUncaughtErrors()
+	log.importUnhandledRejections()
+
+	log.subscribe( x => console.log(x) )
+
+	await mappo.start()
+
+	const app 		= new Application()
+	const router 	= getOakRouter(mappo)
+
+	app.use((ctx : Context,  next: Next) => {
+		ctx.response.headers.set('Access-Control-Allow-Origin', '*')
+		return next()
+	})
+
+	app.use(router.routes())
+	app.use(router.allowedMethods())
+
+	app.addEventListener("listen", () => log.info("Listening on 8099"))
+	app.listen({ port })
+
+	await mappo.updateAll()
 }
-
-const paAdapter = 	new InfoCompassAdapter({
-						meta:	{
-							name: 		"pa-adapter",
-							sourceUrl:	"https://public.socialmap-berlin.de",
-							sourceName:	"socialmap berlin"
-						},
-						url: "https://public.socialmap-berlin.de/items",
-						schedule: scheduleFromCron("*/5 * * * *")
-					})		
-
-const mappo = 	new MappoBackend({
-					storage			: new DenoKvStorage("pa-item-storage"),
-					differ			: new NaiveMappoDiffer(),
-					adapters		: [ paAdapter ],
-				})
-
-const log	=	new LogSubject()
-
-log.importLogsFrom(mappo.log$, "MappoBackend")
-log.importUncaughtErrors()
-log.importUnhandledRejections()
-
-log.subscribe( x => console.log(x) )
-
-await mappo.start()
-
-
-const app = new Application()
-
-const router = getOakRouter(mappo)
-
-app.use((ctx : Context,  next: Next) => {
-  ctx.response.headers.set('Access-Control-Allow-Origin', '*')
-  return next()
-})
-
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-app.addEventListener("listen", () => log.info("Listening on 8099"))
-app.listen({ port: 8099 })
-
-
-await mappo.updateAll()
